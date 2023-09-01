acb72d27a13d438a7f1a46540f769a2f9b69f32e
gameline <108338823+g-ameline@users.noreply.github.com>
2023-09-01 21:16:58 +0300

    Update query_test.go

--- query_test.go
+++ query_test.go
@@ -9,32 +9,89 @@ const path_to_db = "./social-network.db"
 
 func Test_query(t *testing.T) {
 	path_to_nothing := "./nothing"
-	fmt.Println("RIGHT PATH", path_to_db)
-	fmt.Println(Is_database_exist(path_to_db))
-	fmt.Println("\nWRONG PATH", path_to_nothing)
-	fmt.Println(Is_database_exist(path_to_nothing))
+
+	fmt.Println("\n__________________________")
+	fmt.Println("\nTEST GET TABLES NAME")
+	fmt.Println("ok")
 	fmt.Println(Get_tables_name(path_to_db))
+	fmt.Println("wrong")
 	fmt.Println(Get_tables_name(path_to_nothing))
-	fmt.Println("\n update a value  ")
-	fmt.Println("how many rows ?")
-	fmt.Println(Get_ids(path_to_db, "users"))
+
+	fmt.Println("\n__________________________")
+	fmt.Println("\nTEST GET ONE ROW ONE COND")
+	fmt.Println("ok")
+	fmt.Println(Get_row_one_cond(path_to_db, "users", "id", "2"))
+	fmt.Println("wrong")
+	fmt.Println(Get_row_one_cond(path_to_db, "users", "id", "ert"))
+	fmt.Println(Get_row_one_cond(path_to_nothing, "users", "id", "ert"))
+	fmt.Println(Get_row_one_cond(path_to_db, "users", "idasd", "2"))
+
+	fmt.Println("\n__________________________")
+	fmt.Println("\nTEST GET ROWS")
+	fmt.Println("ok")
+	fmt.Println(Get_rows(path_to_db, "users"))
+	fmt.Println("wrong")
+	fmt.Println(Get_rows(path_to_db, "usaaaaars"))
+	fmt.Println(Get_rows(path_to_nothing, "users"))
+
+	fmt.Println("\n__________________________")
+	fmt.Println("\nIS RECORD EXIST")
+	fmt.Println("ok")
 	user_id := "3"
-	fmt.Println("\nwhat about the number", user_id)
-	fmt.Println(Get_row_one_cond(path_to_db, "users", "id", user_id))
-	fmt.Println(Get_ids(path_to_db, "users"))
-	fmt.Println("update output", Update_value(path_to_db, "users", user_id, "session", "same"))
-	fmt.Println(Get_row_one_cond(path_to_db, "users", "id", user_id))
-	user_id = "1"
-	fmt.Println("i\nwhat about the number", user_id)
-	fmt.Println(Get_row_one_cond(path_to_db, "users", "id", user_id))
-	fmt.Println(Get_ids(path_to_db, "users"))
-	fmt.Println("TESTING HERE")
-	fmt.Println(Get_id_one_cond(path_to_db, "users", "session", "same"))
-	res, err := Get_id_one_cond(path_to_db, "users", "session", "prout")
-	fmt.Println("res", res, "err", err)
-	fmt.Println("update output", Update_value(path_to_db, "users", user_id, "session", "same"))
-	fmt.Println(Get_row_one_cond(path_to_db, "users", "id", user_id))
-	fmt.Println("\ntry the det 1 id 2 cond stuff")
-	user_id = "3"
-	fmt.Println(Get_id_two_cond(path_to_db, "users", "id", user_id, "session", "same"))
+	fmt.Println(Is_record_one_cond(path_to_db, "users", "id", user_id))
+	fmt.Println("wrong")
+	fmt.Println(Is_record_one_cond(path_to_db, "users", "idsd", user_id))
+	fmt.Println(Is_record_one_cond(path_to_nothing, "users", "id", user_id))
+	fmt.Println(Is_record_one_cond(path_to_db, "users", "id", "0908099"))
+	fmt.Println(Is_record_one_cond(path_to_db, "users", "id", "NDLSBsd34"))
+
+	fmt.Println("\n__________________________")
+	fmt.Println("\nGET ID 1 COND")
+	fmt.Println("ok")
+	fmt.Println(Get_id_one_cond(path_to_db, "users", "email", "wobi@e.w2"))
+	fmt.Println("wrong")
+	fmt.Println(Get_id_one_cond(path_to_db, "users", "email", "wobi@e.w2sad"))
+	fmt.Println(Get_id_one_cond(path_to_db, "users", "emsdail", "wobi@e.w2sad"))
+	fmt.Println(Get_id_one_cond(path_to_db, "userasds", "email", "wobi@e.w2sad"))
+	fmt.Println(Get_id_one_cond(path_to_nothing, "users", "email", "wobi@e.w2"))
+
+	fmt.Println("\n__________________________")
+	fmt.Println("\nGET ID 2 COND")
+	fmt.Println("ok")
+	fmt.Println(Get_id_two_cond(path_to_db, "users", "email", "wobi@e.w2", "first_name", "uz"))
+	fmt.Println("wrong")
+	fmt.Println(Get_id_two_cond(path_to_db, "users", "email", "wobi@e.w2", "first_name", "uasdz"))
+	fmt.Println(Get_id_two_cond(path_to_db, "users", "email", "wobi@e.w2", "firadst_name", "uz"))
+	fmt.Println(Get_id_two_cond(path_to_db, "users", "email", "wosdbi@e.w2", "first_name", "uz"))
+	fmt.Println(Get_id_two_cond(path_to_db, "users", "esadmail", "wobi@e.w2", "first_name", "uz"))
+	fmt.Println(Get_id_two_cond(path_to_db, "usdasders", "email", "wobi@e.w2", "first_name", "uz"))
+	fmt.Println(Get_id_two_cond(path_to_nothing, "users", "email", "wobi@e.w2", "first_name", "uz"))
+
+	fmt.Println("\n__________________________")
+	fmt.Println("\nINSERT 1 ROW")
+	fmt.Println("ok")
+
+	a_group := map[string]string{}
+	a_group["title"] = "PATATE"
+	a_group["creator_id"] = "1"
+	fmt.Println(Insert_one_row(path_to_db, "groups", a_group))
+
+	// user_one, _ := Get_row_one_cond(path_to_db, "users", "id", "2")
+	// user_x := user_one
+	// delete(user_x, "id")
+	user_x := map[string]string{}
+	user_x["email"] = "balek"
+	user_x["password"] = "ladsa"
+	user_x["first_name"] = "BOOOOBB"
+	user_x["private"] = "1"
+	fmt.Println(Insert_one_row(path_to_db, "users", user_x))
+	fmt.Println(Insert_row(path_to_db, "users", user_x))
+	fmt.Println("wrong")
+	fmt.Println(Insert_one_row(path_to_nothing, "users", user_x))
+	fmt.Println(Insert_one_row(path_to_db, "usesadrs", user_x))
+	delete(user_x, "email")
+	fmt.Println(Insert_one_row(path_to_db, "users", user_x))
+	user_x["email"] = "wobi@e.w2"
+	fmt.Println(Insert_one_row(path_to_db, "users", user_x))
+
 }
